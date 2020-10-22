813b1700aa1880bea04ec57908a781365fb2ee18
gszasza1 <gszasza1@gmail.com>
2020-10-23 00:23:55 +0200

    feat: Add NgRx & Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,63 @@
+import { registerLocaleData } from '@angular/common';
+import localeHu from '@angular/common/locales/hu';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NavigationActionTiming,
+  routerReducer,
+  StoreRouterConnectingModule,
+} from '@ngrx/router-store';
+import { Action, ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 
+// tslint:disable-next-line: no-any
+export function stateSetter(reducer: ActionReducer<any>): ActionReducer<any> {
+  // tslint:disable-next-line: no-any
+  return (state: any, action: any) => {
+    if (action.type === 'SET_ROOT_STATE') {
+      return action.payload;
+    }
+    return reducer(state, action);
+  };
+}
+export const metaReducers: MetaReducer<{}, Action>[] = [stateSetter];
+
+registerLocaleData(localeHu);
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {
+        router: routerReducer,
+      },
+      {
+        metaReducers,
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        },
+      }
+    ),
+    // Connects RouterModule with StoreModule, uses MinimalRouterStateSerializer by default
+    StoreRouterConnectingModule.forRoot({
+      navigationActionTiming: NavigationActionTiming.PostActivation,
+    }),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'hu-HU' },
+    { provide: MAT_DATE_LOCALE, useValue: 'hu-HU' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
