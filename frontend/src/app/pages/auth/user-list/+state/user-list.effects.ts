c0482a4b3fec548a194ebcac45841db19bc0a55d
gszasza1 <gszasza1@gmail.com>
2020-10-23 21:07:08 +0200

    feat: added userlist and initialized it

---
+++ frontend/src/app/pages/auth/user-list/+state/user-list.effects.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { catchError, delay, map, mergeMap, switchMap } from 'rxjs/operators';
+
+import { AuthService } from '../../auth.service';
+import {
+  UserListActionTypes,
+  UserListDeleteError,
+  UserListDeleteRequest,
+  UserListDeleteResponse,
+  UserListError,
+  UserListResponse,
+} from './user-list.acions';
+
+@Injectable()
+export class UserListEffects {
+  @Effect() getUsers$ = this.actions$.pipe(
+    ofType(UserListActionTypes.UserListRequest),
+    mergeMap(() =>
+      this.service.getUserList().pipe(
+        delay(100),
+        map((x) => new UserListResponse(x)),
+        catchError(async () => new UserListError())
+      )
+    )
+  );
+  @Effect() deleteUser$ = this.actions$.pipe(
+    ofType(UserListActionTypes.UserListDeleteRequest),
+    switchMap((action: UserListDeleteRequest) =>
+      this.service.deleteUser(action.payload).pipe(
+        delay(100),
+        map(() => new UserListDeleteResponse()),
+        catchError(async () => new UserListDeleteError())
+      )
+    )
+  );
+
+  constructor(private service: AuthService, private actions$: Actions) {}
+}
---
+++ frontend/src/app/pages/auth/user-list/user-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { UserListDeleteRequest } from './+state/user-list.acions';
+import { User } from './+state/user-list.reducer';
+import { UserListQuery } from './+state/user-list.selector';
+
+@Component({
+  selector: 'anomalia-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss'],
+})
+export class UserListComponent implements OnInit {
+  isRequesting$: Observable<boolean> | undefined;
+  isDeleteRequesting$: Observable<boolean> | undefined;
+  userList$: Observable<User[]> | undefined;
+  deleteId$: Observable<string> | undefined;
+  constructor(private store: Store) {}
+  ngOnInit(): void {
+    this.isRequesting$ = this.store.pipe(
+      select(UserListQuery.getUserListRequesting)
+    );
+    this.isDeleteRequesting$ = this.store.pipe(
+      select(UserListQuery.getUserDeleteRequesting)
+    );
+    this.deleteId$ = this.store.pipe(select(UserListQuery.getDeleteId));
+    this.userList$ = this.store.pipe(select(UserListQuery.getUserList));
+  }
+
+  delete(id: string) {
+    this.store.dispatch(new UserListDeleteRequest(id));
+  }
+}
