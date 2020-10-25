4983ed6a0c1b333e2850b05b9682c847b1ce572a
Juhasz Balazs <juhaszb.98.01@gmail.com>
2020-10-25 14:09:22 +0100

    chore: git clang correct formatting

--- parser/ciff.cpp
+++ parser/ciff.cpp
@@ -7,74 +7,77 @@
 
 #include "exceptions.h"
 
-ciff::ciff(const std::string filename) {
-  std::ifstream ifs{filename, std::ios::binary | std::ios::in | std::ios::ate};
+ciff::ciff(const std::string filename)
+{
+	std::ifstream ifs{ filename,
+			   std::ios::binary | std::ios::in | std::ios::ate };
 
-  if (!ifs.is_open()) {
-    // TODO throw exception
-  }
-  std::istream_iterator<uint8_t> start(ifs), end;
-  std::vector<uint8_t> data(start, end);
-  parse_from_data(data);
+	if (!ifs.is_open()) {
+		// TODO throw exception
+	}
+	std::istream_iterator<uint8_t> start(ifs), end;
+	std::vector<uint8_t> data(start, end);
+	parse_from_data(data);
 }
 
-void ciff::parse_from_data(const std::vector<uint8_t> &data) {
-  char magic_string[5];
-  magic_string[4] = '\0';
+void ciff::parse_from_data(const std::vector<uint8_t> &data)
+{
+	char magic_string[5];
+	magic_string[4] = '\0';
 
-  size_t switch_var = 0;
+	size_t switch_var = 0;
 
-  size_t magic_length = 0;
-  size_t header_length = 0;
-  size_t content_length = 0;
-  size_t width_length = 0;
-  size_t height_length = 0;
-  size_t caption_length = 0;
-  size_t tag_length = 0;
+	size_t magic_length = 0;
+	size_t header_length = 0;
+	size_t content_length = 0;
+	size_t width_length = 0;
+	size_t height_length = 0;
+	size_t caption_length = 0;
+	size_t tag_length = 0;
 
-  for (auto s : data) {
-    switch (p) {
-    case magic: {
-      magic_string[4 - switch_var] = s;
-      switch_var++;
-      if (switch_var == 4) {
-        p = header_s;
-        switch_var = 0;
-        if (std::string{magic_string} != CIFF_MAGIC)
-          throw magic_exception(magic_string);
-      }
-      break;
-    }
-    case header_s: {
-      this->header.header_size += s << ((8 - switch_var));
-      switch_var++;
-      if (switch_var == 8) {
-        p = content_s;
-        switch_var = 0;
-      }
-      break;
-    }
-    case content_s: {
-      this->header.content_size += s << ((8 - switch_var));
-      switch_var++;
-      if (switch_var == 8) {
-        p = w;
-        switch_var = 0;
-      }
-      break;
-    }
-    case w: {
-      this->header.width += s << (8 - switch_var);
-      switch_var++;
-      if (switch_var == 8) {
-        p = h;
-        switch_var = 0;
-      }
-    }
-    }
-  }
+	for (auto s : data) {
+		switch (p) {
+		case magic: {
+			magic_string[4 - switch_var] = s;
+			switch_var++;
+			if (switch_var == 4) {
+				p = header_s;
+				switch_var = 0;
+				if (std::string{ magic_string } != CIFF_MAGIC)
+					throw magic_exception(magic_string);
+			}
+			break;
+		}
+		case header_s: {
+			this->header.header_size += s << ((8 - switch_var));
+			switch_var++;
+			if (switch_var == 8) {
+				p = content_s;
+				switch_var = 0;
+			}
+			break;
+		}
+		case content_s: {
+			this->header.content_size += s << ((8 - switch_var));
+			switch_var++;
+			if (switch_var == 8) {
+				p = w;
+				switch_var = 0;
+			}
+			break;
+		}
+		case w: {
+			this->header.width += s << (8 - switch_var);
+			switch_var++;
+			if (switch_var == 8) {
+				p = h;
+				switch_var = 0;
+			}
+		}
+		}
+	}
 
-  if (p != done) {
-    // TODO throw exception
-  }
+	if (p != done) {
+		// TODO throw exception
+	}
 }
--- parser/exceptions.h
+++ parser/exceptions.h
@@ -5,17 +5,20 @@
 #include <string>
 
 class ciff_exception : public std::exception {
-  const char *what() const throw() = 0;
+	const char *what() const throw() = 0;
 };
 
 class magic_exception : public ciff_exception {
-public:
-  magic_exception(const std::string &magic) : magic{magic} {};
+    public:
+	magic_exception(const std::string &magic) : magic{ magic } {};
 
-  const char *what() const throw() { return magic.c_str(); }
+	const char *what() const throw()
+	{
+		return magic.c_str();
+	}
 
-private:
-  std::string magic;
+    private:
+	std::string magic;
 };
 
 #endif
