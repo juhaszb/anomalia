800a8d9dfb985f01bf2eed64dfdeb522e0b60274
Juhasz Balazs <juhaszb.98.01@gmail.com>
2020-10-25 13:50:17 +0100

    chore: use clang format

--- parser/ciff.h
+++ parser/ciff.h
@@ -1,70 +1,66 @@
 #ifndef __ciff_h_
 #define __ciff_h_
 
+#include <array>
 #include <bits/stdint-uintn.h>
 #include <exception>
+#include <memory>
 #include <string>
 #include <vector>
-#include <array>
-#include <memory>
 
-#define CIFF_MAGIC "CIFF" 
+#define CIFF_MAGIC "CIFF"
 
-
-class ciff
-{
+class ciff {
 public:
-	
-	//! Construct a ciff object from a file
-	/*!
-	 \param filename From where to construct a ciff object
-	*/
-	ciff(const std::string filename);
+  //! Construct a ciff object from a file
+  /*!
+   \param filename From where to construct a ciff object
+  */
+  ciff(const std::string filename);
 
+  //! Construct a ciff object from data
+  /*!
+    \param data From where to construct a ciff object
+   */
+  ciff(std::vector<uint8_t> &&data);
 
-	//! Construct a ciff object from data
-	/*!
-	  \param data From where to construct a ciff object
-	 */
-	ciff(std::vector<uint8_t>&& data);
+  //! Copy constructor
+  ciff(const ciff &c);
 
+  //! Move constructor
+  ciff(ciff &&c);
 
-	//! Copy constructor
-	ciff(const ciff& c);
-
-
-	//! Move constructor
-	ciff(ciff&& c);
-
-	//! Destructor
-	~ciff() = default;
-
-
+  //! Destructor
+  ~ciff() = default;
 
 private:
+  struct ciff_header {
+    uint64_t header_size;  /*!Header size, 8 byte long field.*/
+    uint64_t content_size; /*! Content size, 8 byte long field.*/
+    uint64_t width;        /*!Width of the image, 8 byte long, can be zero.  */
+    uint64_t height;       /*! Height of the image, 8 byte long, can be zero. */
+    std::string caption;   /*! Caption of the image, cannot contain \n */
+    std::vector<std::string> tags; /*! Image tags */
+  };
 
-	struct ciff_header
-	{
-		uint64_t header_size; /*!Header size, 8 byte long field.*/
-		uint64_t content_size; /*! Content size, 8 byte long field.*/
-		uint64_t width; /*!Width of the image, 8 byte long, can be zero.  */
-		uint64_t height;/*! Height of the image, 8 byte long, can be zero. */
-		std::string caption; /*! Caption of the image, cannot contain \n */
-		std::vector<std::string> tags; /*! Image tags */	
-	};
+  ciff_header header;
+  std::unique_ptr<std::vector<uint8_t>> data; /*! Pixel data */
 
-	ciff_header header;
-	std::unique_ptr<std::vector<uint8_t>> data; /*! Pixel data */
+  enum parse_state {
+    magic,
+    header_s,
+    content_s,
+    w,
+    h,
+    capt,
+    tag,
+    dat,
+    done
+  }; /*! Enum for parsing, internal use only */
 
-	enum parse_state {magic, header_s, content_s, w, h, capt, tag, dat, done}; /*! Enum for parsing, internal use only */
+  parse_state p = magic;
 
-	parse_state p = magic;
-
-	void parse_from_data(const std::vector<uint8_t>& data);	
-
+  void parse_from_data(const std::vector<uint8_t> &data);
 };
-
-
-
 
 #endif
--- parser/exceptions.h
+++ parser/exceptions.h
@@ -4,22 +4,18 @@
 #include <exception>
 #include <string>
 
-class ciff_exception: public std::exception
-{
-	const char * what () const throw () = 0;
+class ciff_exception : public std::exception {
+  const char *what() const throw() = 0;
 };
 
-class magic_exception: public ciff_exception
-{
+class magic_exception : public ciff_exception {
 public:
-	magic_exception( const std::string& magic): magic{magic} {};
+  magic_exception(const std::string &magic) : magic{magic} {};
 
-	const char * what () const throw()
-	{
-		return magic.c_str();	
-	}
+  const char *what() const throw() { return magic.c_str(); }
+
 private:
-	std::string magic;
+  std::string magic;
 };
 
 #endif
