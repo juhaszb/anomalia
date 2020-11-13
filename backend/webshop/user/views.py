aa7ac023f8c22fcebde9ac27892ee6b03567ace2
Máhonfai Bálint <mbalint314@gmail.com>
2020-11-13 23:43:00 +0100

    feat: user delete for admin

--- backend/webshop/user/views.py
+++ backend/webshop/user/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
+from rest_framework.generics import DestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
-from rest_framework.generics import ListAPIView
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from user.serializers import UserSerializer
 
 
@@ -31,4 +32,9 @@ def user_logout(request):
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+
+class UserDelete(DestroyAPIView):
+    queryset = User.objects.all()
     permission_classes = [IsAdminUser]
