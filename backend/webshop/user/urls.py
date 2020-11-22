28ca99ec350802c0329c51caa5764e9fb6592a54
Máhonfai Bálint <mbalint314@gmail.com>
2020-11-22 16:27:46 +0100

    fix: user delete url

--- backend/webshop/user/urls.py
+++ backend/webshop/user/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("refresh", TokenRefreshView.as_view()),
     path("logout", views.user_logout),
     path("", views.UserList.as_view()),
-    path("user/<int:pk>", views.UserDelete.as_view()),
+    path("<int:pk>", views.UserDelete.as_view()),
 ]
